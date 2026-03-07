ea8c5e22b43c39311a12682d7f411dcdca159943
Rotno Chandra Das <rotno050@gmail.com>
2026-03-08 00:18:39 +0600

    Close Done

---
+++ scripts/closedissues.js
@@ -0,0 +1,95 @@
+let closedIssues = [];
+
+const closedIssuesCounter = () => {
+    const closedIssuesCount = document.getElementById('closed-issue-count');
+    closedIssuesCount.innerText = closedIssues.length;
+}
+
+const displayClosedIssues = (closedIssues) => {
+    manageLoading(false);
+    const closedIssuesContainer = document.getElementById('closed-issues-container');
+    closedIssuesContainer.innerHTML = "";
+    closedIssues.forEach((issue) => {
+        const closedDiv = document.createElement('div');
+        closedDiv.innerHTML = `
+            <div class="issue-card bg-[#FFFFFF] border-t-4 ${issue.status === "open" ? 'border-t-[#00A96E]' : 'border-t-[#A855F7]'} rounded-sm shadow-[0px_3px_6px_rgba(0,0,0,0.08)]">
+                <div class="border-b border-b-[#E4E4E7] p-4 space-y-4">
+                    <div class="flex items-center justify-between">
+                        <div>
+                            ${issue.priority === "high" || issue.priority === "medium" ? `<img src="../assets/Open-Status.png" alt="Open Status" class="w-6 h-6">` : `<img src="../assets/Closed- Status .png" alt="Closed Status" class="w-6 h-6">`}
+                        </div>
+                        <div>
+                            ${issue.priority === "high" ? 
+                            `<p class="w-24 bg-[#FEECEC] px-3 py-2 flex items-center justify-center rounded-[100px] geist-font font-medium text-[16px] leading-[100%] tracking-[0%] text-[#EF4444] uppercase">${issue.priority}</p>` : 
+                            issue.priority === "medium" ? 
+                            `<p class="w-24 bg-[#FFF6D1] px-3 py-2 flex items-center justify-center rounded-[100px] geist-font font-medium text-[16px] leading-[100%] tracking-[0%] text-[#F59E0B] uppercase">${issue.priority}</p>` : 
+                            `<p class="w-24 bg-[#EEEFF2] px-3 py-2 flex items-center justify-center rounded-[100px] geist-font font-medium text-[16px] leading-[100%] tracking-[0%] text-[#9CA3AF] uppercase">${issue.priority}</p>`}
+                        </div>
+                    </div>
+                    <div>
+                        <h1 class="geist-font font-semibold text-[20px] leading-[auto] tracking-[0%] text-[#000000]">${issue.title}</h1>
+                        <p class="geist-font font-normal text-[16px] leading-[auto] tracking-[0%] text-[#64748B]">${issue.description}</p>
+                    </div>
+                    <div class="flex items-center gap-2 flex-wrap">
+                        ${createLabelElements(issue.labels)}
+                    </div>
+                </div>
+                <div class="p-4 space-y-2">
+                    <div class="flex items-center justify-between">
+                        <p class="geist-font font-normal text-[13px] leading-[auto] tracking-[0%] text-[#64748B]">#${issue.id} by ${issue.author}</p>
+                        <p class="geist-font font-normal text-[13px] leading-[auto] tracking-[0%] text-[#64748B]">${new Date(issue.createdAt).toLocaleDateString()}</p>
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <p class="geist-font font-normal text-[13px] leading-[auto] tracking-[0%] text-[#64748B]">Assignee: ${issue.assignee}</p>
+                        <p class="geist-font font-normal text-[13px] leading-[auto] tracking-[0%] text-[#64748B]">Updated: ${new Date(issue.updatedAt).toLocaleDateString()}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+        closedIssuesContainer.appendChild(closedDiv);
+        closedIssuesCounter();
+    })
+    manageLoading(false);
+};
+
+const pushDataClosed = (issuesData) => {
+    closedIssues.length = 0;
+    issuesData.forEach((element) => {
+        if(element.status === "closed") {
+            closedIssues.push(element);
+        }
+    });
+    displayClosedIssues(closedIssues);
+};
+
+const loadClosedIssues = () => {
+    manageLoading(true);
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((issuesData) => {
+            pushDataClosed(issuesData.data);
+        })
+};
+
+const closedButton = document.getElementById('closed-button');
+closedButton.addEventListener('click', () => {
+    removeActiveButton();
+    closedButton.classList.add("active-button");
+    manageLoading(true);
+    const allIssuesContainer = document.getElementById('all-issues-container');
+    allIssuesContainer.classList.add('hidden');
+    const closedIssuesContainer = document.getElementById('closed-issues-container');
+    closedIssuesContainer.classList.remove('hidden');
+    const openIssuesContainer = document.getElementById('open-issues-container');
+    openIssuesContainer.classList.add('hidden');
+    const allCount = document.getElementById('all-count');
+    allCount.classList.add('hidden');
+    const openCount = document.getElementById('open-count');
+    openCount.classList.add('hidden');
+    const closedCount = document.getElementById('closed-count');
+    closedCount.classList.remove('hidden');
+    loadClosedIssues();
+});
