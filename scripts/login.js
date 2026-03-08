914572bffb841570729175937a293feb1d03709e
Rotno Chandra Das <rotno050@gmail.com>
2026-03-09 01:27:48 +0600

    D

--- scripts/login.js
+++ scripts/login.js
@@ -3,8 +3,7 @@ signInButton.addEventListener('click', () => {
     const userName = document.getElementById('input-username').value;
     const password = document.getElementById('input-password').value;
     if(userName === 'admin' && password === 'admin123') {
-        alert('Login successful!');
-        window.location.assign('../pages/home.html');
+        window.location.assign('./home.html');
     }
     else {
         if(userName !== 'admin' && password !== 'admin123') {
