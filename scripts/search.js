61a3ffec7547a74ad6aa41993c6b77a62e21fe91
Rotno Chandra Das <rotno050@gmail.com>
2026-03-09 17:21:03 +0600

    Done

--- scripts/search.js
+++ scripts/search.js
@@ -1,44 +1,46 @@
 const searchButton = document.getElementById('search-button');
 searchButton.addEventListener('click', () => {
-    removeActiveButton();
-    manageLoading(true);
     const searchInput = document.getElementById('search-input');
     const searchValue = searchInput.value.trim().toLowerCase();
+
     if(searchValue === "") {
         alert("Please enter a search term!");
-        loadAllIssues();
-        const allButton = document.getElementById("all-button");
-        allButton.classList.add("active-button");
         return;
     }
+
+    manageLoading(true);
+    removeActiveButton();
+    // document.getElementById('all-button').classList.add('active-button');
+    document.getElementById('all-issues-container').classList.remove('hidden');
+    document.getElementById('all-issues-container').classList.add('grid');
+    document.getElementById('open-issues-container').classList.add('hidden');
+    document.getElementById('open-issues-container').classList.remove('grid');
+    document.getElementById('closed-issues-container').classList.add('hidden');
+    document.getElementById('closed-issues-container').classList.remove('grid');
+    document.getElementById('all-count').classList.remove('hidden');
+    document.getElementById('open-count').classList.add('hidden');
+    document.getElementById('closed-count').classList.add('hidden');
+
     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchValue}`;
     fetch(url)
-        .then((response) => {
-            return response.json();
-        })
+        .then((response) => response.json())
         .then((searchResults) => {
-            const allData = searchResults.data;
-            const filteredData = allData.filter((data) => {
-                const titleMatch = data.title.toLowerCase().includes(searchValue);
-                return titleMatch;
-            });
+            const filteredData = searchResults.data.filter(data => data.title.toLowerCase().includes(searchValue));
             const number = filteredData.length;
-            const allCount = document.getElementById('all-issue-count');
-            allCount.innerText = number;
+
             if(number === 0) {
-                const allIssuesContainer = document.getElementById('all-issues-container');
-                allIssuesContainer.innerHTML = `
+                document.getElementById('all-issues-container').innerHTML = `
                     <div class="flex justify-center items-center text-2xl font-bold text-red-500">
                         <p class="text-center">No issues found</p>
                     </div>
                 `;
+                document.getElementById('all-issue-count').innerText = 0;
                 manageLoading(false);
                 clearSearchValue();
                 return;
             }
             displayAllIssues(filteredData);
-            manageLoading(false);
-        })
+        });
 });
 
 const clearSearchValue = () => {
